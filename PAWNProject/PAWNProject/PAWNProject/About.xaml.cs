dabf6b31216f38938fa4ee9d5eff7866bf653239
astandro <astandro.tc@gmail.com>
2014-05-27 02:38:41 +0700

    sedikit tambahan di bagian fitur

--- PAWNProject/PAWNProject/PAWNProject/About.xaml.cs
+++ PAWNProject/PAWNProject/PAWNProject/About.xaml.cs
@@ -16,5 +16,10 @@ namespace PAWNProject
         {
             InitializeComponent();
         }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
     }
 }
