44a5feb121624bc308edb26833070ad694aea3fe
astandro <astandro.tc@gmail.com>
2014-06-11 17:45:25 +0700

    nih udah ada control classnya semoga

---
+++ PAWNProject/PAWNProject/PAWNProject/Control.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAWNProject
+{
+    //Semua logic code dan controller ada di sini
+    public static class Control
+    {
+        public static List<SoalPsikotes> LoadSoalFromDB(DBSoalContext db)
+        {
+            List<SoalPsikotes> listSoal = new List<SoalPsikotes>();
+            db.CreateIfNotExists();
+            db.LogDebug = true;
+            listSoal = db.SoalPsikotes.ToList();
+            return listSoal;
+        }
+
+        public static List<SoalPsikotes> LoadSoalLogikaAngkaFromDB(DBSoalContext db)
+        {
+            List<SoalPsikotes> listSoal = new List<SoalPsikotes>();
+            db.CreateIfNotExists();
+            db.LogDebug = true;
+            listSoal = (from soal in db.SoalPsikotes
+                        where soal.Jenis_Soal.Equals("Logika Angka")
+                        select soal).ToList();
+            return listSoal;
+        }
+
+        public static List<SoalPsikotes> LoadSoalLogikaFormilFromDB(DBSoalContext db)
+        {
+            List<SoalPsikotes> listSoal = new List<SoalPsikotes>();
+            db.CreateIfNotExists();
+            db.LogDebug = true;
+            listSoal = (from soal in db.SoalPsikotes
+                        where soal.Jenis_Soal.Equals("Logika Formil")
+                        select soal).ToList();
+            return listSoal;
+        }
+
+        public static List<SoalPsikotes> LoadSoalPadananKataFromDB(DBSoalContext db)
+        {
+            List<SoalPsikotes> listSoal = new List<SoalPsikotes>();
+            db.CreateIfNotExists();
+            db.LogDebug = true;
+            listSoal = (from soal in db.SoalPsikotes
+                        where soal.Jenis_Soal.Equals("Padanan Kata")
+                        select soal).ToList();
+            return listSoal;
+        }
+
+        public static double HitungSkor(int benar, List<SoalPsikotes> listSoal)
+        {
+            return ((double)benar / listSoal.Count) * 100;
+        }
+
+        public static int HitungBenar(List<Jawaban> jawabanUser, List<SoalPsikotes> listSoal)
+        {
+            int benar = 0;
+            foreach (Jawaban item in jawabanUser)
+            {
+                foreach (SoalPsikotes soal in listSoal)
+                {
+                    if (item.jawaban.ToArray().First() == soal.JawabanBenar.ToArray().First() && item.kodeSoal.Equals(soal.Kode_Soal))
+                    {
+                        benar++;
+                    }
+                }
+            }
+            return benar;
+        }
+    }
+}
